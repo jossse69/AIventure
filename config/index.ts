6174229413499c12f6294c4ad7c6b8ea15e7ced0
jossse69 <78055979+jossse69@users.noreply.github.com>
2023-07-01 11:25:13 -0300

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,17 +1,17 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = ''
-export const API_KEY = ''
+export const APP_ID = '650f20a4-8897-4c41-b372-dab34e287b37'
+export const API_KEY = 'app-D1TtGLmnby1ZCl4arrGaqubn'
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
+  title: 'AIventure',
+  description: 'a RPG ai game',
   copyright: '',
   privacy_policy: '',
   default_language: 'zh-Hans',
 }
 
 export const isShowPrompt = false
-export const promptTemplate = 'I want you to act as a javascript console.'
+export const promptTemplate = 'you are a rpg narrator, respond to player responses in a {{tone}} tone'
 
 export const API_PREFIX = '/api'
 
